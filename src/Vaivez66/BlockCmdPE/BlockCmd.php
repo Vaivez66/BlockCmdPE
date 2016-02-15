70378123850d8a9ff831ac985c84cbd6c5fb9fa1
Reziq Vavai <kakakvavai66@gmail.com>
2016-02-15 11:27:30 +0700

    update

---
+++ src/Vaivez66/BlockCmdPE/BlockCmd.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ASUS
+ * Date: 15/02/2016
+ * Time: 9:46
+ */
+namespace Vaivez66\BlockCmdPE;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as TF;
+
+class BlockCmd extends PluginBase{
+
+    public $cfg;
+    private $format;
+
+    public function onEnable(){
+        $this->saveDefaultConfig();
+        $this->getServer()->getLogger()->info(TF::GREEN . 'BlockCmdPE is ready!');
+        $this->getServer()->getPluginManager()->registerEvents(new BlockCmdListener($this), $this);
+        $this->cfg = new Config($this->getDataFolder() . 'config.yml', Config::YAML, array());
+        $this->format = new BlockCmdFormat($this);
+    }
+
+    /**
+     * @return array
+     */
+
+    public function getCmd(){
+        return (array)$this->cfg->get('block.cmd');
+    }
+
+    /**
+     * @return mixed
+     */
+
+    public function getMsg(){
+        $m = $this->cfg->get('block.message');
+        $m = $this->getFormat()->translate($m);
+        return $m;
+    }
+
+    /**
+     * @return mixed
+     */
+
+    public function getFormat(){
+        return $this->format;
+    }
+
+    public function onDisable(){
+        $this->getServer()->getLogger()->info(TF::RED . 'BlockCmdPE was disabled!');
+    }
+
+}
---
+++ src/Vaivez66/BlockCmdPE/BlockCmdListener.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ASUS
+ * Date: 15/02/2016
+ * Time: 9:52
+ */
+namespace Vaivez66\BlockCmdPE;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+
+class BlockCmdListener implements Listener{
+
+    public function __construct(BlockCmd $plugin){
+        $this->plugin = $plugin;
+    }
+
+    public function onCmd(PlayerCommandPreprocessEvent $event){
+        $p = $event->getPlayer();
+        $msg = $event->getMessage();
+        if($p->hasPermission('block.cmd.pe.bypass')){
+            return;
+        }
+        foreach($this->plugin->getCmd() as $cmd){
+            if(stripos($msg, $cmd) === 0){
+                $event->setCancelled(true);
+                $p->sendMessage($this->plugin->getMsg());
+            }
+        }
+    }
+
+}
